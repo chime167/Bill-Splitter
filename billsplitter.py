bb961505c71d4ac80d0839eff2886480271830bb
chime167 <98487737+chime167@users.noreply.github.com>
2022-09-08 23:40:44 +0000

    Delete billsplitter.py

--- billsplitter.py
+++
@@ -1,9 +0,0 @@
-#!usr/bin/env python3
-
-number_of_friends = int(input('Enter the number of friends joining (including you):\n'))
-if number_of_friends > 0:
-    print('Enter the name of every friend (including you), each on a new line:')
-    friends = {input(): 0 for _ in range(number_of_friends)}
-    print(f'\n{friends}')
-else:
-    print('\nNo one is joining for the party')
